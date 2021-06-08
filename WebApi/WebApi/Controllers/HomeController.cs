3c118f1d287e9a5a8376f93d82ca2e5bce0995f8
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-06-08 15:34:40 +0200

    Revert "Revert "API added""
    This reverts commit 539dd999d805acfde1e1a91ef9b87d1a536c842f.

---
+++ WebApi/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("Home")]
+    public class HomeController : ControllerBase //Dodanie bazy danych
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("All")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            List<string> list = new List<string>();
+            list.Add("XD");
+            list.Add("XD");
+            list.Add("XD");
+            list.Add("XD");
+            list.Add("XD");
+            list.Add("XD");
+            list.Add("XD");
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> AddProduct([FromBody] object przychodzaceDane)
+        {
+            List<object> list = new List<object>();
+
+            return Ok(list);
+        }
+
+        [HttpGet]
+        [Route("Get/{Id}")]
+        public async Task<IActionResult> GetUser([FromRoute] int Id)
+        {
+            return Ok();
+        }
+    }
+}
