fef36962a6adb5550758a389f994d7b5a4ea50a1
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-06-10 12:51:31 +0200

    Clients read and delete operations added and improved appearance of the application

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom'
 import SearchBar from "./components/searchBar"
-import BooksPage from "./components/booksPage"
+import MainPage from "./components/mainPage"
 import ClientsPage from "./components/clientsPage"
 import OrdersPage from "./components/ordersPage"
 import BookEdit from "./components/bookEdit"
+import ClientEdit from "./components/clientEdit"
 
 function App() {
   return (
@@ -14,15 +15,15 @@ function App() {
           <div>
             <SearchBar />
           </div>
-          <Route exact path="/" component={BooksPage} />
+          <Route exact path="/" component={MainPage} />
           <Route exact path="/Clients" component={ClientsPage} />
           <Route exact path="/Orders" component={OrdersPage} />
-          <Route path="/BookEdit"
-            render={(props) => <BookEdit {...props} />} />
+          <Route path="/BookEdit" render={(props) => <BookEdit {...props} />} />
+          <Route path="/ClientEdit" render={(props) => <ClientEdit {...props} />} />
         </BrowserRouter>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
